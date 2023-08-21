0c1f467c2418a531015076852e8f2e891b7a8cf6
Kyle Washington <kmw1993@msn.com>
2023-08-21 01:49:42 -0400

    Added the Models with musiclibrary class and Data with application Db context class

---
+++ MusicLibraryAPI/MusicLibraryAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MusicLibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicLibraryAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<MusicLibrary> MusicLibraries { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+
+    }
+}
